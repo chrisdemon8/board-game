ca0a14a5fc058c4435dbb3a67000ac6218732df7
valczi123 <valczi@orange.fr>
2021-12-07 21:45:51 +0100

    Désole j'ai mal merge

--- src/App/bdd/MyConnection.php
+++ src/App/bdd/MyConnection.php
@@ -6,7 +6,7 @@ use \PDO;
 
 use Framework\Config\Config;
 
-class MyConnection //extends PDO
+class MyConnection extends PDO
 {
     private static $connection=null;
 
@@ -22,7 +22,7 @@ class MyConnection //extends PDO
     public static function getInstance() {
  
         if(is_null(self::$connection)) {
-          self::$connection = new PDO ('mysql:host='.Config::get('HOST').';dbname='.Config::get('DBNAME'),Config::get('USERNAME'), Config::get('PASSWORD'));
+          self::$connection = new PDO ('mysql:host=localhost;dbname='.Config::get('DBNAME'),Config::get('USERNAME'), Config::get('PASSWORD'));
           self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         }
 
