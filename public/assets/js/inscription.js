fbb213920ea797ad4931d620ca0d722ffab4651c
chrisdemon8 <christophepetre57@gmail.com>
2021-12-13 15:23:13 +0100

    PETRE 13-12-2021

--- public/assets/js/inscription.js
+++ public/assets/js/inscription.js
@@ -8,11 +8,14 @@ let passwordFieldBis = document.getElementById("passwordBis");
 let registerButton = document.getElementById("register");
 
 
+
+passwordField.addEventListener("keyup", changeInput);
+
 passwordFieldBis.addEventListener("keyup", changeInput);
 
 function changeInput(evt) {
 
-    if (evt.currentTarget.id === "passwordBis") {
+    if (evt.currentTarget.id === "passwordBis" || evt.currentTarget.id === "password") {
         let password = passwordField.value;
         let passwordBis = passwordFieldBis.value;
         let helperText = passwordFieldBis.nextElementSibling;
@@ -44,59 +47,79 @@ function submitForm() {
         'email': email,
         'password': password
     }
+ 
 
-    console.log('test', jsonData);
+    if (password === passwordBis) {
+
+        fetch("AddUser", {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    jsonData
+                }),
+                mode: 'cors',
+                cache: 'default'
+            }).then(function (response) {
+
+                if (response.ok)
+                    response.text().then(function (res) {
+                        if (res === 1)
+                            window.location.href = "/connexion";
+                        else {
+                            console.log(res);
+                            if (res.includes("ERROR_MAIL")) { 
+                                let helperText = emailField.nextElementSibling;
+                                console.log(helperText);
+                                helperText.textContent = "Email déjà utilisé";
+                                helperText.style.display = 'block';
+                            } else if (res.includes("INVALID_MAIL")) {
+                                let helperText = emailField.nextElementSibling; 
+                                helperText.textContent = "Email non conforme";
+                                helperText.style.display = 'block';
+                            } else {
+                                let helperText = emailField.nextElementSibling;
+                                helperText.style.display = 'none';
+                            }
 
 
-    fetch("AddUser", {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                jsonData
-            }),
-            mode: 'cors',
-            cache: 'default'
-        }).then(function (response) {
+                            if (res.includes("ERROR_USERNAME")) { 
+                                let helperText = usernameField.nextElementSibling;
+                                helperText.textContent = "Pseudo déjà utilisé";
+                                helperText.style.display = 'block';
+                            } else {
+                                let helperText = usernameField.nextElementSibling;
+                                helperText.style.display = 'none';
+                            }
 
-            if (response.ok)
-                response.text().then(function (res) {
-                    if (res === 1)
-                        window.location.href = "/connexion";
-                    else {
-                        console.log(res);
-                        if (res.includes("ERROR_MAIL")) {
-                            console.log("ERROR_MAIL");
-                            let helperText = emailField.nextElementSibling;
-                            console.log(helperText);
-                            helperText.textContent = "Email déjà utilisé";
-                            helperText.style.display = 'block';
-                        } else if (res.includes("INVALID_MAIL")) {
-                            let helperText = emailField.nextElementSibling;
-                            console.log(helperText);
-                            helperText.textContent = "Email non conforme";
-                            helperText.style.display = 'block';
-                        } else {
-                            let helperText = emailField.nextElementSibling;
-                            helperText.style.display = 'none';
+                            if (res.includes("INVALID_FIRST_NAME")) {
+                                let helperText = firstNameField.nextElementSibling;
+                                helperText.textContent = "Format du prénom : uniquement des lettres";
+                                helperText.style.display = 'block';
+                            } else {
+                                let helperText = firstNameField.nextElementSibling;
+                                helperText.style.display = 'none';
+                            }
+
+                            if (res.includes("INVALID_LASTNAME")) {
+                                let helperText = firstNameField.nextElementSibling;
+                                helperText.textContent = "Format du nom de famille : uniquement des lettres";
+                                helperText.style.display = 'block';
+                            } else {
+                                let helperText = firstNameField.nextElementSibling;
+                                helperText.style.display = 'none';
+                            }
                         }
+                    })
+            })
+            .catch(function (error) {
+                console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+            });
 
-
-                        if (res.includes("ERROR_USERNAME")) {
-                            console.log("ERROR_USERNAME");
-                            let helperText = usernameField.nextElementSibling;
-                            helperText.textContent = "Pseudo déjà utilisé";
-                            helperText.style.display = 'block';
-                        } else {
-                            let helperText = usernameField.nextElementSibling;
-                            helperText.style.display = 'none';
-                        }
-                    }
-                })
-        })
-        .catch(function (error) {
-            console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
-        });
+    } else {
+        let helperText = passwordFieldBis.nextElementSibling;
+        helperText.style.display = 'block';
+    }
 
 }
